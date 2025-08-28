599e37fd3d093ce00e56c2ee7a2281bad3ccd999
Manasseh Oruebor <manassehoruebor@gmail.com>
2025-08-28 15:18:44 +0100

    Feat: Debugged Productand Category route

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  controllers: [ProductController],   // ✅ Controllers only
+  controllers: [ProductController], // ✅ Controllers only
   providers: [ProductService, PrismaService], // ✅ Providers (services)
-  exports: [ProductService],          // ✅ Export if you’ll use it elsewhere
+  imports: [PrismaModule], // ✅ Export if you’ll use it elsewhere
 })
 export class ProductModule {}
-
